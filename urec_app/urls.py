839887839936e927f115cb78e5b60db85f34642c
Isaac <43579566+isaacdyess@users.noreply.github.com>
2023-04-19 14:08:38 -0500

    Fixed several styling issues and added frontend elements for login.

--- urec_app/urls.py
+++ urec_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
+from django.contrib import admin
 from . import views
 from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
     path('', views.home, name='home'),
 
+    path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 
     path('accident/', views.accident, name='accident'),
