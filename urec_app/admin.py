9d36a9e6ec799c5dc3d74c431da8fe83cc4a41fb
Matthew Lane <matthewglenn@protonmail.com>
2024-02-21 21:51:33 -0600

    Make admin site more user friendly
    Create custom admin site featuring UREC name
    Add verbose_name to urec_app
    Add verbose_name to all report models
    Add verbose_name_plural to all applicable report models
    Add get_full_name to contact model
    Add string returns to all report models
    Integrate incidents/injuries and witness/patient contacts into reports on admin site
    Set date_time_submission in code instead of automatically to allow for editing in admin site

--- urec_app/admin.py
+++ urec_app/admin.py
@@ -1,20 +1,61 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-admin.site.register(UrecUser)
 
+# Register your models here.
+
+
+admin.site.register(UrecUser)
 admin.site.register(UrecFacility)
 admin.site.register(UrecLocation)
-
-admin.site.register(InjuryIllnessReport)
-admin.site.register(InjuryIllnessReportInjury)
-admin.site.register(InjuryIllnessReportContactPatient)
-admin.site.register(InjuryIllnessReportContactWitness)
-
-admin.site.register(IncidentReport)
-admin.site.register(IncidentReportIncident)
-admin.site.register(IncidentReportContactPatient)
-admin.site.register(IncidentReportContactWitness)
-
 admin.site.register(Count)
+
+
+class InjuryIllnessReportContactPatientInline(admin.StackedInline):
+    model = InjuryIllnessReportContactPatient
+    extra = 0
+    max_num = 1
+    can_delete = False
+
+
+class InjuryIllnessReportContactWitnessInline(admin.StackedInline):
+    model = InjuryIllnessReportContactWitness
+    extra = 0
+
+
+class InjuryIllnessReportInjuryInline(admin.StackedInline):
+    model = InjuryIllnessReportInjury
+    extra = 0
+
+
+class InjuryIllnessReportAdmin(admin.ModelAdmin):
+    inlines = [InjuryIllnessReportContactPatientInline, InjuryIllnessReportInjuryInline,
+               InjuryIllnessReportContactWitnessInline]
+
+
+admin.site.register(InjuryIllnessReport, InjuryIllnessReportAdmin)
+
+
+class IncidentReportContactPatientInline(admin.StackedInline):
+    model = IncidentReportContactPatient
+    extra = 0
+    max_num = 1
+    can_delete = False
+
+
+class IncidentReportContactWitnessInline(admin.StackedInline):
+    model = IncidentReportContactWitness
+    extra = 0
+
+
+class IncidentReportIncidentInline(admin.StackedInline):
+    model = IncidentReportIncident
+    extra = 0
+
+
+class IncidentReportAdmin(admin.ModelAdmin):
+    inlines = [IncidentReportContactPatientInline, IncidentReportIncidentInline,
+               IncidentReportContactWitnessInline]
+
+
+admin.site.register(IncidentReport, IncidentReportAdmin)
